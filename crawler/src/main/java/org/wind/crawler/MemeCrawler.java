54b247453149b4fb68be9ee9ed0739f13733f8f0
woniu <woniu@in66.com>
2017-05-20 19:15:38 +0800

    build the base module

---
+++ crawler/src/main/java/org/wind/crawler/MemeCrawler.java
@@ -0,0 +1,8 @@
+package org.wind.crawler;
+
+/**
+ * Created by stephen
+ * on 2017/5/20.
+ */
+public class MemeCrawler {
+}
---
+++ heart/src/main/java/org/wind/heart/controller/TestController.java
@@ -0,0 +1,28 @@
+package org.wind.heart.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by stephen
+ * on 2017/5/20.
+ */
+
+@Controller
+@EnableAutoConfiguration
+public class TestController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(TestController.class, args);
+    }
+
+}
